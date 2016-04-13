678628badd810686b1989d502af44dcd34821d00
Swagnik Dutta <swagnikd@gmail.com>
2016-04-13 12:56:08 +0530

    Create ABA12D.cpp

---
+++ ABA12D.cpp
@@ -0,0 +1,78 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define N 1000000
+
+int primes[78498];
+int divisorSum[N+1];
+bool isPrime[N+1];
+bool isK[N];
+
+int X[N+1];
+int Cumulative[N+1];
+
+void pre()
+{
+    int pos=0;
+    for(int i=2;i<=N;i++)
+    {
+        if(isPrime[i])
+        {
+            primes[pos++]=i;
+            divisorSum[i]=i+1;
+
+            for(int j=i+i ; j<=N ; j+=i)
+            {
+                isPrime[j]=false;
+                divisorSum[j]+=i;
+            }
+        }
+        else
+        {
+            divisorSum[i]+=(i+1);
+
+            for(int j=i+i ; j<=N ; j+=i)
+            {
+                divisorSum[j]+=i;
+            }
+        }
+    }
+
+    isPrime[0]=isPrime[1]=false;
+    divisorSum[0]=0;divisorSum[1]=1;
+    //cumulative
+
+    for(int i=2;i<=N;i++)
+        if( isPrime[divisorSum[i]] )
+            X[i]=1;
+
+    for(int i=2;i<=N;i++)
+        Cumulative[i]=Cumulative[i-1]+X[i];
+/*
+    for(int i=0;i<=10;i++)
+        cout<<X[i]<<" "<< Cumulative[i]<<endl;
+*/
+
+}
+
+
+int main()
+{
+    memset(isPrime, true , sizeof isPrime);
+    pre();
+
+
+    int counter=0;
+    int t,L,R;
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%d%d",&L,&R);
+
+            int ans=Cumulative[R]-Cumulative[L-1];
+            cout<<ans<<endl;
+
+    }
+    
+    return 0;
+}
